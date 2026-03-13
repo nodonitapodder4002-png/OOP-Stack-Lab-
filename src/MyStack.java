6f0e0a012360a6839525e5067dd72be2701e7e10
Addri Podder <podder242-35-403@diu.edu.bd>
2026-03-13 22:29:32 +0600

    MyStack implementation using array

---
+++ src/MyStack.java
@@ -0,0 +1,40 @@
+public class MyStack {
+
+    private int stackPointer;
+    private int[] elements;
+
+    public MyStack(int size) {
+        elements = new int[size];
+        stackPointer = -1;
+    }
+
+    public int top() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        return elements[stackPointer];
+    }
+
+    public void push(int n) {
+        if (stackPointer == elements.length - 1) {
+            throw new RuntimeException("Stack is full");
+        }
+        stackPointer++;
+        elements[stackPointer] = n;
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        stackPointer--;
+    }
+
+    public boolean isEmpty() {
+        return stackPointer == -1;
+    }
+
+    public int getSize() {
+        return stackPointer + 1;
+    }
+}
